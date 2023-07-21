6f5323a84581917c26e31cad65af8540a5baa668
Jaren Nawy <jarennawy@gmail.com>
2023-07-21 12:59:04 -0400

    added styles

--- app/page.tsx
+++ app/page.tsx
@@ -51,64 +51,80 @@ const RegisterForm = () => {
   };
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleSubmit(handleRegistration)}>
-      <div className="flex-col gap-4">
-        <label>Name</label>
+    <form className="flex flex-col gap-4 p-4 bg-[#202A3C]" onSubmit={handleSubmit(handleRegistration)}>
+      <div className="flex flex-col gap-4">
+        <label className="text-white font-semibold" htmlFor="name">
+          Name
+        </label>
         <input
-          className="text-black"
+          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
           //@ts-ignore
+
           name="name"
           type="text"
-          {...register('name', { required: "Name is required" })}
+          {...register("name", { required: "Name is required" })}
           onChange={handleChange}
         />
-        <small className="text-danger">
+        <small className="text-red-700">
+
           {errors?.name && errors.name.message}
         </small>
       </div>
-      <div>
-        <label>Email</label>
+      <div className="flex flex-col gap-4">
+        <label className="text-white font-semibold" htmlFor="email">
+          Email
+        </label>
         <input
-          type="email"
-          className="text-black"
+          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
           //@ts-ignore
+
           name="email"
-          {...register('email', { required: "Email is required" })}
+          type="email"
+          {...register("email", { required: "Email is required" })}
           onChange={handleChange}
         />
-        <small className="text-danger">
+        <small className="text-red-700">
           {errors?.email && errors.email.message}
         </small>
       </div>
-      <div>
-        <label>Password</label>
+      <div className="flex flex-col gap-4">
+        <label className="text-white font-semibold" htmlFor="password">
+          Password
+        </label>
         <input
-          type="password"
-          className="text-black"
+          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
           //@ts-ignore
           name="password"
-          {...register('password', {
+          type="password"
+          {...register("password", {
             required: "Password is required",
             minLength: {
               value: 8,
-              message: "Password must have at least 8 characters"
-            }
+              message: "Password must have at least 8 characters",
+            },
           })}
           onChange={handleChange}
         />
-        <small className="text-danger">
+        <small className="text-red-700">
           {errors?.password && errors.password.message}
         </small>
       </div>
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="mt-8 flex w-full items-center justify-center space-x-2 rounded-lg bg-primary px-4 py-2 text-lg font-bold text-white hover:bg-white hover:text-primary hover:text-[#202A3C] hover:duration-500 hover:ease-in-out"
+      >
+        <p>Submit</p>
+      </button>
     </form>
   );
 };
 
 function page() {
   return (
-    <div><RegisterForm /></div>
-  )
+    <div className="bg-blue-50 p-8">
+      <RegisterForm />
+    </div>
+  );
 }
 
 export default page;
