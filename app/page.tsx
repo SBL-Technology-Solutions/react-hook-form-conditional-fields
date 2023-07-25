73f5ec58eb4c5f06530013a211d96dde7e2785ce
Jaren Nawy <jarennawy@gmail.com>
2023-07-25 10:30:55 -0400

    added Toast, simulated api call, button loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { debounce } from "lodash";
 import { z } from "zod";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const schema = z.object({
   name: z.string().nonempty({ message: "Name is required" }),
@@ -11,19 +13,27 @@ const schema = z.object({
   password: z.string().min(8, { message: "Password must have at least 8 characters" }),
 });
 
-const debouncedSave = debounce((data) => {
-  const validationResult = schema.safeParse(data);
-  if (validationResult.success) {
-    saveForm(data);
-  }
-}, 5000);
 //@ts-ignore
 const saveForm = (data) => {
   console.log("Saving form data:", data);
 };
 
 const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
+
+  const debouncedSave = debounce((data) => {
+    const validationResult = schema.safeParse(data);
+    if (validationResult.success) {
+      saveForm(data);
+      setIsLoading(false);
+      toast.success(`Autosaved successfully! Autosaved form state: ${JSON.stringify(data, null, 2)}`, {
+        autoClose: 2000,
+      });
+    } else {
+      handleError(validationResult.error.formErrors);
+    }
+  }, 5000);
 
   useEffect(() => {
     return () => {
@@ -31,15 +41,22 @@ const RegisterForm = () => {
     };
   }, []);
 //@ts-ignore
-  const handleRegistration = (data) => {
-    const validationResult = schema.safeParse(data);
-    if (validationResult.success) {
-      console.log("Valid form data:", validationResult.data);
-      // Perform your form submission here
-    } else {
-      handleError(validationResult.error.formErrors);
-    }
-  };
+const handleRegistration = (data) => {
+  const validationResult = schema.safeParse(data);
+  if (validationResult.success) {
+    console.log("Valid form data:", validationResult.data);
+    setIsLoading(true);
+    setTimeout(() => {
+      saveForm(data);
+      setIsLoading(false);
+      toast.success(`Saved successfully! Form state: ${JSON.stringify(data, null, 2)}`, {
+        autoClose: 3000,
+      });
+    }, 1000);
+  } else {
+    handleError(validationResult.error.formErrors);
+  }
+};
 //@ts-ignore
   const handleError = (formErrors) => {
     console.log("Form errors:", formErrors);
@@ -111,9 +128,37 @@ const RegisterForm = () => {
       </div>
       <button
         type="submit"
-        className="mt-8 flex w-full items-center justify-center space-x-2 rounded-lg bg-primary px-4 py-2 text-lg font-bold text-white hover:bg-white hover:text-primary hover:text-[#202A3C] hover:duration-500 hover:ease-in-out"
+        className={`mt-8 flex w-full items-center justify-center space-x-2 rounded-lg bg-primary px-4 py-2 text-lg font-bold text-white hover:bg-white hover:text-primary hover:text-[#202A3C] hover:duration-500 hover:ease-in-out ${
+          isLoading ? "cursor-wait" : ""}`}
       >
-        <p>Submit</p>
+        {isLoading ? (
+          <>
+            <span>Loading...</span>
+            {}
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-2-5.291A7.962 7.962 0 0120 12h4c0-3.042-1.135-5.824-3-7.938l-3 2.647z"
+              />
+            </svg>
+          </>
+        ) : (
+          <p>Submit</p>
+        )}
       </button>
     </form>
   );
@@ -123,6 +168,7 @@ function page() {
   return (
     <div className="bg-blue-50 p-8">
       <RegisterForm />
+      <ToastContainer /> {}
     </div>
   );
 }
