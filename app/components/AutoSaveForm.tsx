9a72fb43ae71e9026ec96362d4eaa1928e7395ce
Shane Liebenthal <shaneliebenthal@gmail.com>
2023-07-26 10:21:20 -0400

    Cutover AutoSaveFormTest to AutoSaveForm, remove heavy lodash dependency as we are using ts-hooks debounce instead

--- app/components/AutoSaveForm.tsx
+++ app/components/AutoSaveForm.tsx
@@ -3,11 +3,11 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { debounce } from "lodash";
 import { z } from "zod";
 import { toast } from "react-toastify";
 import { Button } from "@/components/ui/Button";
 import { Loader2, Send } from "lucide-react";
+import { useDebounce } from "usehooks-ts";
 
 const formSchema = z.object({
   name: z.string().nonempty({ message: "Name is required" }),
@@ -15,9 +15,9 @@ const formSchema = z.object({
   message: z.string().nonempty({ message: "Message is required" }),
 });
 
-//@ts-ignore
-const saveForm = (data) => {
-  console.log("Saving form data:", data);
+const mockedAPICall = async (timeout: number = 2000) => {
+  console.log("making an API request");
+  await new Promise((resolve) => setTimeout(resolve, timeout));
 };
 
 export const AutoSaveForm = () => {
@@ -25,127 +25,117 @@ export const AutoSaveForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    getValues,
+    formState: { errors, isDirty, dirtyFields },
+    watch,
+    reset,
   } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     mode: "all",
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
   });
 
-  const debouncedSave = debounce((data) => {
-    const validationResult = formSchema.safeParse(data);
-    if (validationResult.success) {
-      saveForm(data);
-      setIsLoading(false);
-      toast.success(
-        `Autosaved successfully! Autosaved form state: ${JSON.stringify(
-          data,
-          null,
-          2
-        )}`,
-        {
-          autoClose: 2000,
-        }
-      );
-    } else {
-      handleError(validationResult.error.formErrors);
-    }
-  }, 5000);
+  const debouncedValue = useDebounce(watch(), 1000);
+  const DebouncedValueStringified = JSON.stringify(debouncedValue);
 
   useEffect(() => {
-    return () => {
-      debouncedSave.cancel();
-    };
-  }, []);
-  //@ts-ignore
-  const handleRegistration = (data) => {
-    const validationResult = formSchema.safeParse(data);
-    if (validationResult.success) {
-      console.log("Valid form data:", validationResult.data);
-      setIsLoading(true);
-      setTimeout(() => {
-        saveForm(data);
-        setIsLoading(false);
+    console.log("save is triggered, checking if isDirty is true");
+    const debouncedSave = async () => {
+      if (isDirty) {
+        console.log("isDirty is true, saving form data");
+        setIsLoading(true);
+        //simulate API call and wait 2 seconds
+        await mockedAPICall(2000);
         toast.success(
-          `Saved successfully! Form state: ${JSON.stringify(data, null, 2)}`,
+          `Autosaving form state: ${JSON.stringify(debouncedValue, null, 2)}`,
           {
             autoClose: 3000,
           }
         );
-      }, 1000);
-    } else {
-      handleError(validationResult.error.formErrors);
-    }
-  };
-  //@ts-ignore
-  const handleError = (formErrors) => {
-    console.log("Form errors:", formErrors);
-    // Handle form errors here
-  };
+        reset({ ...debouncedValue });
+        setIsLoading(false);
+      }
+    };
 
-  const handleChange = () => {
-    debouncedSave(getValues());
+    debouncedSave();
+    // We are explicitly using only the debouncedValue as a dependency because we only want this useEffect to run when the debouncedValue changes.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [DebouncedValueStringified]);
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log("submitting form data:", data);
+    toast.success(`Form submitted successfully!`, {
+      autoClose: 2000,
+    });
   };
 
   return (
-    <form
-      className="flex flex-col gap-4 p-4 bg-[#202A3C]"
-      onSubmit={handleSubmit(handleRegistration)}
-    >
-      <div className="flex flex-col gap-4">
-        <label className="text-white font-semibold" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
-          type="text"
-          {...register("name")}
-          onChange={handleChange}
-        />
-        <small className="text-red-700">
-          {errors?.name && errors.name.message}
-        </small>
+    <div className="p-4">
+      <div className="flex flex-col gap-2 text-white font-semibold">
+        <span>isDirty: {isDirty ? "yes" : "no"}</span>
+        <span>Dirty Fields: {JSON.stringify(dirtyFields, null, 2)}</span>
+        <span>Debounced Value: {JSON.stringify(debouncedValue, null, 2)}</span>
       </div>
-      <div className="flex flex-col gap-4">
-        <label className="text-white font-semibold" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
-          type="email"
-          {...register("email")}
-          onChange={handleChange}
-        />
-        <small className="text-red-700">
-          {errors?.email && errors.email.message}
-        </small>
-      </div>
-      <div className="flex flex-col gap-4">
-        <label className="text-white font-semibold" htmlFor="message">
-          Message
-        </label>
-        <textarea
-          className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
-          {...register("message")}
-          onChange={handleChange}
-        />
-        <small className="text-red-700">
-          {errors?.message && errors.message.message}
-        </small>
-      </div>
-      <Button type="submit" className="text-base" disabled={isLoading}>
-        {isLoading ? (
-          <>
-            <Loader2 className="animate-spin" />{" "}
-            <span className="px-2">Saving...</span>
-          </>
-        ) : (
-          <>
-            <Send />
-            <span className="font-bold px-2">Submit</span>
-          </>
-        )}
-      </Button>
-    </form>
+
+      <form
+        className="flex flex-col gap-4 py-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col gap-4">
+          <label className="text-white font-semibold" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            type="text"
+            {...register("name")}
+          />
+          <small className="text-red-700">
+            {errors?.name && errors.name.message}
+          </small>
+        </div>
+        <div className="flex flex-col gap-4">
+          <label className="text-white font-semibold" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            type="email"
+            {...register("email")}
+          />
+          <small className="text-red-700">
+            {errors?.email && errors.email.message}
+          </small>
+        </div>
+        <div className="flex flex-col gap-4">
+          <label className="text-white font-semibold" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            {...register("message")}
+          />
+          <small className="text-red-700">
+            {errors?.message && errors.message.message}
+          </small>
+        </div>
+        <Button type="submit" className="text-base" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <Loader2 className="animate-spin" />{" "}
+              <span className="px-2">Saving...</span>
+            </>
+          ) : (
+            <>
+              <Send />
+              <span className="font-bold px-2">Submit</span>
+            </>
+          )}
+        </Button>
+      </form>
+    </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { ToastContainer } from "react-toastify";
 import { AutoSaveForm } from "./components/AutoSaveForm";
 import "react-toastify/dist/ReactToastify.css";
-import { AutoSaveFormTest } from "./components/AutoSaveFormTest";
 
 function page() {
   return (
     <div className="bg-slate-800 p-8">
-      {/* <AutoSaveForm /> */}
-      <AutoSaveFormTest />
+      <AutoSaveForm />
       <ToastContainer />
     </div>
   );
