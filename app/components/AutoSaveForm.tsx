54db81eb26748d112b6ab132b87bff2e1c426ca4
Shane Liebenthal <shaneliebenthal@gmail.com>
2023-07-25 23:51:25 -0400

    Install Shadcn UI, add Shadcn Button component and update/simplify isLoading button state, add icons in both button states

--- app/components/AutoSaveForm.tsx
+++ app/components/AutoSaveForm.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { debounce } from "lodash";
 import { z } from "zod";
 import { toast } from "react-toastify";
+import { Button } from "@/components/ui/Button";
+import { Loader2, Send } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().nonempty({ message: "Name is required" }),
@@ -131,41 +133,19 @@ export const AutoSaveForm = () => {
           {errors?.message && errors.message.message}
         </small>
       </div>
-      <button
-        type="submit"
-        className={`mt-8 flex w-full items-center justify-center space-x-2 rounded-lg bg-primary px-4 py-2 text-lg font-bold text-white hover:bg-white hover:text-primary hover:text-[#202A3C] hover:duration-500 hover:ease-in-out ${
-          isLoading ? "cursor-wait" : ""
-        }`}
-      >
+      <Button type="submit" className="text-base" disabled={isLoading}>
         {isLoading ? (
           <>
-            <span>Loading...</span>
-            {}
-            <svg
-              className="animate-spin h-5 w-5 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              />
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-2-5.291A7.962 7.962 0 0120 12h4c0-3.042-1.135-5.824-3-7.938l-3 2.647z"
-              />
-            </svg>
+            <Loader2 className="animate-spin" />{" "}
+            <span className="px-2">Saving...</span>
           </>
         ) : (
-          <p>Submit</p>
+          <>
+            <Send />
+            <span className="font-bold px-2">Submit</span>
+          </>
         )}
-      </button>
+      </Button>
     </form>
   );
 };
