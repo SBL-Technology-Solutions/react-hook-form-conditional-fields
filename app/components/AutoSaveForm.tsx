d70d7ad87be053b19030a90e9f19939da8f65ee9
Shane Liebenthal <shaneliebenthal@gmail.com>
2023-07-31 18:22:25 -0400

    Add ThemeProvider to set dark mode by default, cleanup AutoSaveForm styling - dark form fields, conditional error message span, tighter spacing, make debouncedValue output generic instead of hardcoding keys and values

--- app/components/AutoSaveForm.tsx
+++ app/components/AutoSaveForm.tsx
@@ -9,7 +9,7 @@ import { Loader2, Send } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
 import { useAutoSave } from "@/hooks/useAutoSave";
 
-export const formSchema = z.object({
+const formSchema = z.object({
   name: z.string().nonempty({ message: "Name is required" }),
   email: z.string().email({ message: "Email is required" }),
   message: z.string().nonempty({ message: "Message is required" }),
@@ -51,9 +51,11 @@ export const AutoSaveForm = () => {
       title: "Autosaved Form",
       description: (
         <div className="flex flex-col gap-2 text-sm">
-          <span>Name: {debouncedValue.name}</span>
-          <span>Email: {debouncedValue.email}</span>
-          <span>Message: {debouncedValue.message}</span>
+          {Object.entries(debouncedValue).map(([key, value]) => (
+            <span key={key}>
+              {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+            </span>
+          ))}
         </div>
       ),
     });
@@ -82,42 +84,52 @@ export const AutoSaveForm = () => {
         className="flex flex-col gap-4 py-8"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <label className="text-white font-semibold" htmlFor="name">
             Name
           </label>
           <input
-            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            className="w-full rounded bg-slate-800 px-2 py-1 text-white"
             type="text"
             {...register("name")}
           />
-          <small className="text-red-700">{errors?.name?.message}</small>
+          {errors.name && (
+            <span className="text-red-600 text-sm">{errors.name?.message}</span>
+          )}
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <label className="text-white font-semibold" htmlFor="email">
             Email
           </label>
           <input
-            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            className="w-full rounded bg-slate-800 px-2 py-1 text-white"
             type="email"
             {...register("email")}
           />
-          <small className="text-red-700">{errors?.email?.message}</small>
+          {errors.email && (
+            <span className="text-red-600 text-sm">
+              {errors.email?.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <label className="text-white font-semibold" htmlFor="message">
             Message
           </label>
           <textarea
-            className="mt-2 w-full rounded bg-white px-2 py-1 text-black"
+            className="w-full rounded bg-slate-800 px-2 py-1 text-white"
             {...register("message")}
           />
-          <small className="text-red-700">{errors?.message?.message}</small>
+          {errors.message && (
+            <span className="text-red-600 text-sm">
+              {errors.message?.message}
+            </span>
+          )}
         </div>
         <Button
           variant="default"
           type="submit"
-          className="my-4 text-base hover:scale-105 transition-transform duration-200 active:scale-95 focus-visible:scale-95"
+          className="my-8 text-base hover:scale-105 transition-transform duration-200 active:scale-95 focus-visible:scale-95"
           disabled={isLoading}
         >
           {isLoading ? (
